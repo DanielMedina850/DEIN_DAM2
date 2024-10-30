fdab4f41b844448d4fa58be06bd9d263d17ffbcd
DanielMedina850 <dmedinacol1@educacion.navarra.es>
2024-10-30 14:03:59 +0100

    ACTIVIDAD 1.8

---
+++ UT2/A1.8_Diseño_Controles/A1.8_Diseño_Controles/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace A1._8_Diseño_Controles
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<Estudiante> estudiantes = new List<Estudiante>();
+            estudiantes.Add(new Estudiante() { id = 1, name = "Daniel", apellido1 = "Medina", apellido2 = "Colmenares" });
+            estudiantes.Add(new Estudiante() { id = 2, name = "Aimar", apellido1 = "Huici", apellido2 = "" });
+            estudiantes.Add(new Estudiante() { id = 3, name = "Sergio", apellido1 = "Sajona", apellido2 = "" });
+
+            alumnos.ItemsSource = estudiantes;
+           
+        }
+
+
+        public class Estudiante
+        {
+            public int id { get; set; }
+
+            public string name { get; set; }
+
+            public string apellido1 { get; set; }
+
+            public string apellido2 { get; set; }
+        }
+
+        private void Peliculas_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
