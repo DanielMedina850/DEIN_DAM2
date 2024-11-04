4dd9af59eb747ceb32cfdc9ca51151ddfa18766d
DanielMedina850 <dmedinacol1@educacion.navarra.es>
2024-11-04 20:33:41 +0100

    tarea A1.10

--- UT2/Actividad1.10_Formulario_Empleados/Actividad1.10_Formulario_Empleados/MainWindow.xaml.cs
+++ UT2/Actividad1.10_Formulario_Empleados/Actividad1.10_Formulario_Empleados/MainWindow.xaml.cs
@@ -16,9 +16,43 @@ namespace Actividad1._10_Formulario_Empleados
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Empleado> empleados;
         public MainWindow()
         {
             InitializeComponent();
+
+            empleados = new List<Empleado>();
+            Datagrid.ItemsSource = empleados;
         }
+
+        private void Guardar_Click(object sender, RoutedEventArgs e)
+        {
+
+            var nuevoEmpleado = new Empleado
+            {
+                Nombre = txtNombre.Text,
+                Apellidos = txtApellidos.Text,
+                Email = txtEmail.Text,
+                Telefono = txtTelefono.Text
+            };
+
+            empleados.Add(nuevoEmpleado);
+
+
+            Datagrid.Items.Refresh();
+
+            txtNombre.Clear();
+            txtApellidos.Clear();
+            txtEmail.Clear();
+            txtTelefono.Clear();
+        }
+    }
+
+    public class Empleado
+    {
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
     }
 }
