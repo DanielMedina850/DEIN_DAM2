0085b48b1316b99ca00788e3f95993e103dbb17e
DanielMedina850 <dmedinacol1@educacion.navarra.es>
2024-11-06 14:07:12 +0100

    ejericicio dein

---
+++ UT2/A1.11_Aplicación_Orla_de_clase/A1.11_Aplicación_Orla_de_clase/MainWindow.xaml.cs
@@ -0,0 +1,195 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace A1._11_Aplicación_Orla_de_clase
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MouseEnter1(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "Paula Iturbide Cristobal";
+        }
+        private void MouseLeave1(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "";
+        }
+
+        private void MouseEnter2(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "Paula Iturbide Cristobal";
+        }
+        private void MouseLeave2(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "";
+        }
+
+        private void MouseEnter3(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "Paula Iturbide Cristobal";
+        }
+        private void MouseLeave3(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "";
+        }
+
+        private void MouseEnter4(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "Paula Iturbide Cristobal";
+        }
+        private void MouseLeave5(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "";
+        }
+
+        private void MouseEnter6(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "Paula Iturbide Cristobal";
+        }
+        private void MouseLeave6(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "";
+        }
+
+        private void MouseEnter7(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "Paula Iturbide Cristobal";
+        }
+        private void MouseLeave7(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "";
+        }
+
+        private void MouseEnter8(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "Paula Iturbide Cristobal";
+        }
+        private void MouseLeave8(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "";
+        }
+
+        private void MouseEnter9(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "Paula Iturbide Cristobal";
+        }
+        private void MouseLeave9(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "";
+        }
+
+        private void MouseEnter10(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "Paula Iturbide Cristobal";
+        }
+        private void MouseLeave10(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "";
+        }
+
+        private void MouseEnter11(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "Paula Iturbide Cristobal";
+        }
+        private void MouseLeave11(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "";
+        }
+
+        private void MouseEnter12(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "Paula Iturbide Cristobal";
+        }
+        private void MouseLeave12(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "";
+        }
+
+        private void MouseEnter13(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "Paula Iturbide Cristobal";
+        }
+        private void MouseLeave13(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "";
+        }
+
+        private void MouseEnter14(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "Paula Iturbide Cristobal";
+        }
+        private void MouseLeave14(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "";
+        }
+
+        private void MouseEnter15(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "Paula Iturbide Cristobal";
+        }
+        private void MouseLeave15(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "";
+        }
+
+        private void MouseEnter16(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "Paula Iturbide Cristobal";
+        }
+        private void MouseLeave16(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "";
+        }
+
+        private void MouseEnter17(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "Paula Iturbide Cristobal";
+        }
+        private void MouseLeave17(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "";
+        }
+
+        private void MouseEnter18(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "Paula Iturbide Cristobal";
+        }
+        private void MouseLeave18(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "";
+        }
+
+        private void MouseEnter19(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "Paula Iturbide Cristobal";
+        }
+        private void MouseLeave19(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "";
+        }
+
+        private void MouseEnter20(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "Paula Iturbide Cristobal";
+        }
+        private void MouseLeave20(object sender, MouseEventArgs e) 
+        {
+            LabelAlumno1.Text = "";
+        }
+    }
+}
