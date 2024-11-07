f1225f6865e5e990cc282474ba76059246ec5edf
DanielMedina850 <dmedinacol1@educacion.navarra.es>
2024-11-07 12:20:15 +0100

    agregando la BD

---
+++ UT2/1.12_BdD_Medina_Daniel_Ajona_Sergio/1.12_BdD_Medina_Daniel_Ajona_Sergio/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _1._12_BdD_Medina_Daniel_Ajona_Sergio
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ UT2/1.12_BdD_Medina_Daniel_Ajona_Sergio/1.12_BdD_Medina_Daniel_Ajona_Sergio/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows.Media.Animation;
+
+namespace _1._12_BdD_Medina_Daniel_Ajona_Sergio
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SqlConnection SqlConnection;
+        public MainWindow()
+        {
+            InitializeComponent();
+            string miConexion = ConfigurationManager.ConnectionStrings["_1._12_BdD_Medina_Daniel_Ajona_Sergio.Properties.Settings.BdD_Medina_Daniel_Ajona_SergioConnectionString"].ConnectionString;
+            SqlConnection = new SqlConnection(miConexion);
+            muestraCiclos();
+
+        }
+
+        private void muestraCiclos() 
+        {
+            string consulta = "SELECT * FROM CICLOS";
+            SqlDataAdapter adapter = new SqlDataAdapter(consulta, SqlConnection);
+            using (adapter)
+            {
+
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+
+                listaCiclos.DisplayMemberPath = "Nombre";
+                listaCiclos.SelectedValuePath = "Id";
+                listaCiclos.ItemsSource = dt.DefaultView;
+            }
+        }
+    }
+}
