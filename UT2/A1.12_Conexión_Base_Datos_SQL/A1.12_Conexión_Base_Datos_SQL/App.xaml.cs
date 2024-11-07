f229a0ebd58fe0f229cc4e418748f818cc351184
DanielMedina850 <dmedinacol1@educacion.navarra.es>
2024-11-07 09:37:14 +0100

    j

---
+++ UT2/A1.12_Conexión_Base_Datos_SQL/A1.12_Conexión_Base_Datos_SQL/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace A1._12_Conexión_Base_Datos_SQL
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
