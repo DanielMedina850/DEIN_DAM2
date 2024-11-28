9d7fd5b8fe6b53cd226a326cce45d25850980d1e
DanielMedina850 <dmedinacol1@educacion.navarra.es>
2024-11-28 12:03:43 +0100

    i

---
+++ UT2/DEIN ChatGPT/Ejemplos Moodle/Controles/Controles/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Controles
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<string> lista = new List<string>();
+            lista.Add("Wifi");
+
+            cmbColors.ItemsSource = lista;
+
+
+
+            List<Persona> items = new List<Persona>();
+            items.Add(new Persona() { Nombre = "John Doe", Edad = 42, Mail = "john@doe-family.com" });
+            items.Add(new Persona() { Nombre = "Jane Doe", Edad = 39, Mail = "jane@doe-family.com" });
+            items.Add(new Persona() { Nombre = "Sammy Doe", Edad = 7, Mail = "sammy.doe@gmail.com" });
+            listaViweUsuarios.ItemsSource = items;
+        }
+
+
+        public class Persona
+        {
+            public string Nombre { get; set; }
+
+            public int Edad { get; set; }
+
+            public string Mail { get; set; }
+        }
+    }
+}
---
+++ UT2/EXAMEN/DanielMedinaExamenWPF/DanielMedinaExamenWPF/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DanielMedinaExamenWPF
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private int idProductos;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<Producto> products = new List<Producto>();
+            Producto producto1 = new Producto() { id = 1, name = "Televisor", precio = 500, cantidad = 10 };
+            Producto producto2 = new Producto() { id = 2, name = "Silla", precio = 49, cantidad = 20 };
+            idProductos = 2;
+            dataGridProductos.Items.Add(producto1);
+            dataGridProductos.Items.Add(producto2);
+
+        }
+
+        
+
+        public class Producto
+        {
+            public int id { get; set; }
+
+            public string name { get; set; }
+
+            public int precio { get; set; }
+
+            public Double cantidad { get; set; }
+        }
+
+        private void Button_Click_Guardar(object sender, RoutedEventArgs e)
+        {
+
+            if (int.TryParse(txtPrecio?.Text, out int precio))
+            {
+
+                Producto producto = new Producto() { id = idProductos, name = txtProducto.Text, precio = precio, cantidad = sliderControl.Value };
+                idProductos++;
+                dataGridProductos.Items.Add(producto);
+                txtProducto.Text = "";
+                txtPrecio.Text = "";
+                sliderControl.Value = 0;
+            }
+            else
+            {
+                MessageBox.Show("Por favor, introduce un precio válido.");
+            }
+
+        }
+
+        private void Button_Click_Limpiar(object sender, RoutedEventArgs e)
+        {
+            txtProducto.Text = "";
+            txtPrecio.Text = "";
+            sliderControl.Value = 0;
+        }
+    }
+}
